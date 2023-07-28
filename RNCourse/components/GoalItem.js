c082a834142136e3a6344ec77ef13d4338d292f2
steliosT <steliosevoix@gmail.com>
2023-07-28 11:31:36 +0300

    android ripple & ios

--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -10,31 +10,35 @@ function GoalItem(props) {
 
   console.log("props", props);
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }} // (Android) this make the an effect to the button when you press it
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => (pressed ? styles.pressedItem : "")} // (IOS) this make the an effect to the button when you press it
+      >
         <Text style={styles.goalText}>{props.itemData}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
-    flex: 1,
-    flexDirection: "row",
+    // flex: 1,
+    // flexDirection: "row",
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
-    alignItems: "center",
+    // alignItems: "center",
   },
   goalText: {
-    flex: 1,
+    // flex: 1,
+    padding: 8,
     color: "white",
     textAlign: "center",
   },
-  deleteBtn: {
-    width: "100%",
+  pressedItem: {
+    opacity: 0.5,
   },
 });
 
