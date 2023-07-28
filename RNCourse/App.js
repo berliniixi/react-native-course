85b2966405e3cdc6e0b867158ae5cec18f3b2c96
steliosT <steliosevoix@gmail.com>
2023-07-28 14:08:35 +0300

    initial commit

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,83 +1,21 @@
-import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
-
-export default function App() {
-  const [courseGoal, setCourseGoal] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const enableModal = () => {
-    setModalVisible(true);
-  };
-
-  const disenableModal = () => {
-    setModalVisible(false);
-  };
-
-  const addGoalHandler = (enteredGoalText) => {
-    setCourseGoal((currentCourseGoal) => [
-      ...currentCourseGoal,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    disenableModal();
-  };
-
-  const deleteGoalHandler = (itemID) => {
-    console.log("itemID", itemID);
-    setCourseGoal((currentCourseGoal) =>
-      currentCourseGoal.filter((item) => item.id !== itemID)
-    );
-    console.log("DELETE");
-  };
-
+function App() {
   return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button title="Add New Goal" color="#a065ec" onPress={enableModal} />
-
-        <GoalInput
-          onAddGoal={addGoalHandler}
-          onVisibleModal={modalVisible}
-          onCancel={disenableModal}
-        />
-
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoal}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  itemData={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandler}
-                />
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.id;
-              // console.log(item.text);
-            }}
-          />
-        </View>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.textContainer}>Hello, there</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
-    paddingTop: 80,
-    paddingHorizontal: 16,
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
-
-  goalsContainer: {
-    flex: 5,
-    paddingTop: 20,
-  },
+  textContainer: {},
 });
+
+export default App;
